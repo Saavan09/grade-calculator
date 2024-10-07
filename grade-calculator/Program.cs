126cb2e87e66f908f07c1b1390856c52c7fa0b2c
aceofcolours <saavantandon@gmail.com>
2024-10-07 10:22:12 -0400

    added variables and loop to print student's grades

--- grade-calculator/Program.cs
+++ grade-calculator/Program.cs
@@ -1,9 +1,26 @@
-﻿using System;
+﻿/*
+
+Name: Saavan Tandon
+Class: 8am Tues/Fri with Nick
+Due Date: Sunday 10/13
+
+*/
+
+using System;
 
 class Program 
 {
     public static void Main(string[] args)
     {
-        
+        string name = "Saavan";
+        int[] grades = {100, 92, 87, 200, -20, 52, 82, 75, 67, 88};
+
+        Console.WriteLine("Welcome to the student portal, " + name + "!");
+        Console.WriteLine("Here are your grades: ");
+
+        for(int i=0; i<grades.Length; i++)
+        {
+            Console.Write(grades[i] + " ");
+        }
     }
 }
--- grade-calculator/obj/Debug/net8.0/grade-calculator.AssemblyInfo.cs
+++ grade-calculator/obj/Debug/net8.0/grade-calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("grade-calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+393d9198bcb3d5f5a11f6d069784e6f37fb4b25f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c3bb25e14bda953892033298c963a74995c71d")]
 [assembly: System.Reflection.AssemblyProductAttribute("grade-calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("grade-calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
